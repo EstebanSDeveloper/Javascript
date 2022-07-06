d3ba04bd31493b105044b7e035f3e6912be5be17
esteban sarmiento <esteban.sarmientop@gmail.com>
2022-07-05 21:11:51 -0500

    Desafio 1 Simulador Interactivo

---
+++ Clase-5-2_Desafio_Simulador_Interactivo/js/aplicacion.js
@@ -0,0 +1,44 @@
+// variables globales 
+
+let stock = 10;
+let opcion;
+
+//Funciones
+
+const registrarVenta = (cant) => {
+    if (stock >= cant){
+        stock = stock - cant;
+        console.log(`El nuevo stock es de ${stock} unidades.`);
+    } else {
+        console.log(`No hay suficiente stock, quedan ${stock} unidades.`);
+    }
+}
+
+const reponer = (cant) => {
+    stock = stock + cant;
+    console.log(`El nuevo stock es de ${stock} unidades.`)
+}
+
+//Menu
+do {
+    opcion = Number(prompt("Ingrese una opcion: \n\n1 - Registrar una venta \n2 - Reponer \n3 - Salir"));
+    let cant;
+
+    switch (opcion) {
+        case 1:
+            cant = Number(prompt("Ingrese la cantidad de unidades que se vendieron."));
+            registrarVenta(cant);
+            break;
+        case 2:
+            cant = Number(prompt("Ingrese la cantidad de unidades que ingresaron."));
+            reponer(cant);
+            break;
+        case 3:
+            alert("Hasta luego");
+            break;
+        default:
+            alert("Opcion incorrecta");
+            break;
+    }
+
+} while (opcion !== 3);
